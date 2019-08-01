1af474ac7cc1ff66c939ce47e57c838d333d1d2a
ManuCas <manucastle@hotmail.com>
2019-07-31 22:41:54 -0500

    another try to deploy

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.get('/send', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
