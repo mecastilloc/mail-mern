684df559a2af2ea55ffb2d6120f6c0ab20f8cc0c
ManuCas <manucastle@hotmail.com>
2019-08-01 23:32:20 -0500

    aded react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import MailForm from './pages/Mail';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <MailForm />
-      </div>
+      // <div className="App">
+      //     <MailForm />
+      // </div>
+      <Router>
+      {/* <Navbar /> */}
+        <Switch>
+        <Route exact path="/" component={MailForm} />
+        {/* <Route exact path="/signup" component={Signup} />
+        <Route exact path="/members" component={Members} /> */}
+        </Switch>
+    </Router>
     );
   }
 }
